69ac5861617b913b9a517738abab9f3e2af260e8
yail259 <yaoke259@outlook.com>
2023-07-23 22:21:35 +1000

    added blurb

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,6 @@
     export let data;
     const postPaths = data.postPaths;
 
-    let mc: HTMLDivElement;
-
-    function handleMCMove(event: any) {
-        if (event.code === "ArrowDown") {
-            mc.style.top = `${mc.offsetTop - 50 + 20}px`;
-        } else if (event.code === "ArrowUp") {
-            // console.log(`${mc.offsetTop - 10}px`);
-
-            mc.style.top = `${mc.offsetTop - 70}px`;
-            // console.log(mc.offsetTop);
-        } else if (event.code === "ArrowLeft") {
-            mc.style.left = `${mc.offsetLeft - 10}px`;
-        } else if (event.code === "ArrowRight") {
-            mc.style.left = `${mc.offsetLeft + 10}px`;
-        }
-
-        console.log(event);
-    }
-
 </script>
 
 {#each postPaths as path}
@@ -28,25 +9,11 @@
     <br>
 {/each}
 
-<svelte:window on:keydown={handleMCMove} />
+A Software Engineering and Psychology student at USYD.
 
-<div class="balls" id="blue" bind:this={mc}>
-</div>
+Interested in AI, Web Development, Education, Abnormal Psychology and Philosophy.
 
-<style>
-    .balls {
-        border-radius: 50%;
-        position: fixed;
-        width: 50px;
-        height: 50px;
-        margin-top: 50px;
-    }
 
-    #blue {
-        background-color: blue;
-        left: 0;
-    }
-</style>
 
 
 
